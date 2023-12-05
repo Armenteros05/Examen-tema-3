f4117ab291ea40212893810f5de6090cc39e64c7
Armenteros05 <sarmerod@myuax.com>
2023-12-05 11:36:35 +0100

    reflexion y mejoras

---
+++ reflexion y mejoras.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Basandome en mi experiencia, creo que para el futuro la implementacion de sistemas operativos que puedan procesar una mayor carga de datos, como los ordenadores cuanticos permitiran una mejora exponencial en el desarrollo y analisis de datos permitiendo tablas de valores que recojan muchos mas parametros y que permitiran procesar mejor y mas rapido funciones avanzadas" << std::endl;
+
+    return 0;
+}
